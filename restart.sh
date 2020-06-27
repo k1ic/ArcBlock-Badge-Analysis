da095fdd645186def0d7341d1ff3e64006d71d92
k1ic <chenkaill@hotmail.com>
2020-06-27 15:40:49 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
+#killall nginx;/usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
 ps -ef | grep gunicorn | grep -v grep | awk '{ print "kill -9 " $2}' | sh
 gunicorn -c gunicorn_8080.conf server:app --timeout 60
